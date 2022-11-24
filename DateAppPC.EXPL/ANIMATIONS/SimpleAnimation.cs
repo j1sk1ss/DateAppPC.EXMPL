03ad4e3c388e8ff3152bb8f0bb1fa0a561933057
j1sk1ss <fotikolay222@gmail.com>
2022-11-24 17:00:34 +0500

    some syntax cleaning

---
+++ DateAppPC.EXPL/ANIMATIONS/SimpleAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace DateAppPC.EXPL.ANIMATIONS {
+    public class SimpleAnimation {
+        public SimpleAnimation() {
+            DoubleAnimation = new DoubleAnimation();
+        }
+        private DoubleAnimation DoubleAnimation { get; set; }
+
+        public void MoveGrid(Grid grid, int firstPos, int secondPos, int duration) {
+            DoubleAnimation.From = firstPos;
+            DoubleAnimation.To = secondPos;
+            DoubleAnimation.Duration = TimeSpan.FromSeconds(duration);
+            grid.BeginAnimation(FrameworkElement.HeightProperty, DoubleAnimation);
+        }
+    }
+}
--- DateAppPC.EXPL/WINDOWS/UserInfo.xaml.cs
+++ DateAppPC.EXPL/WINDOWS/UserInfo.xaml.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.IO;
-using System.Net;
+using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Media.Imaging;
+using DateAppPC.EXPL.ANIMATIONS;
 using DateAppPC.EXPL.OBJECTS;
 using Microsoft.Win32;
 
@@ -11,67 +11,46 @@ namespace DateAppPC.EXPL.WINDOWS {
     public partial class UserInfo : Window {
         public UserInfo(MainWindow mainWindow, User user) {
             InitializeComponent();
-            MainWindow = mainWindow;
-            User = user;
-            
+
+            MainWindow    = mainWindow;
+            User          = user;
             UserName.Text = user.Name;
             NickName.Text = user.Nick;
-
+            Info.Text     = user.Info;
+            
             foreach (var interest in user.Interests) {
                 Interests.Text += interest + ", ";
             }
-
-            Info.Text = user.Info;
         }
         private MainWindow MainWindow { get; set; }
         private string LinkToPicture { get; set; }
         private User User { get; set; }
         private void ClosedWindow(object sender, EventArgs e) {
-            try {
-                MainWindow.ProfileImage.Source = new BitmapImage();
-                
-                var tempLink = "C:\\Users\\j1sk1ss\\RiderProjects\\DateAppPC.EXPL" +
-                               $"\\DateAppPC.EXPL\\IMAGES\\{User.Name}_{User.Nick}.jpg";
-                if (File.Exists(tempLink)) {
-                    File.Delete(tempLink);
-                }
-
-                User.Name = UserName.Text;
-                User.Nick = NickName.Text;
-                User.Info = Info.Text;
-
-                var temp = Interests.Text.Replace(" ", "").Split(",");
-                User.Interests.Clear();
-                foreach (var interest in temp) {
-                    User.Interests.Add(interest);
-                }
-
-                var link = $"C:\\Users\\j1sk1ss\\RiderProjects\\DateAppPC.EXPL\\" +
-                           $"DateAppPC.EXPL\\IMAGES\\{UserName.Text}_{NickName.Text}.jpg";
-                File.Copy(LinkToPicture, link, true);
-                User.ProfileImage = link;
-                User.DateOfBirth  = Age.DisplayDate;
-                
-                MainWindow.User = User;
-                MainWindow.UpdateProfile();
-            }
-            catch (Exception exception) {
-                MessageBox.Show($"{exception}");
-                throw;
-            }
+            User.Name      = UserName.Text;
+            User.Nick      = NickName.Text;
+            User.Info      = Info.Text;
+            User.Interests = Interests.Text.Replace(" ", "").Split(",").ToList();
+            
+            var link = $"C:\\Users\\j1sk1ss\\RiderProjects\\DateAppPC.EXPL\\" +
+                       $"DateAppPC.EXPL\\IMAGES\\{UserName.Text}_{NickName.Text}.jpg";
+            File.Copy(LinkToPicture, link, true);
+            
+            User.ProfileImage = link;
+            User.DateOfBirth  = Age.DisplayDate;
+            MainWindow.User   = User;
+            
+            MainWindow.UpdateProfile();
         }
-
         private void PictureChose(object sender, RoutedEventArgs e) {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
+            var openFileDialog = new OpenFileDialog {
+                Filter = "jpg files (*.jpg)|*.jpg|All files (*.*)|*.*",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            };
+            if (openFileDialog.ShowDialog() != true) return;
             
-            openFileDialog.InitialDirectory = "c:\\";
-            openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
-            openFileDialog.FilterIndex = 2;
-            openFileDialog.RestoreDirectory = true;
-            
-            if (openFileDialog.ShowDialog() == true) {
-                LinkToPicture = openFileDialog.FileName;
-            }
+            LinkToPicture         = openFileDialog.FileName;
+            PreviewPicture.Source = new BitmapImage(new Uri(LinkToPicture));
         }
     }
 }
